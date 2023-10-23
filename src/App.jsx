5d47b5a57090e9a357109f714f2973e687a15fca
Dev Aryan Dogra <devdogra1@gmail.com>
2023-10-23 12:39:13 +0530

    Create static menu page

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,40 @@ import Container from '@mui/material/Container'
 import Paper from '@mui/material/Paper'
 import Navbar from './components/Navbar/Navbar'
 import MenuItemCard from './components/MenuItemCard/MenuItemCard'
+import Grid from '@mui/material/Unstable_Grid2'
+import Breadcrumbs from '@mui/material/Breadcrumbs'
+import Link from '@mui/material/Link'
 
 function App() {
 
   return (
     <>
       <Navbar />
-      <Container sx={{ bgcolor: 'tomato', p:3}}>
-        <MenuItemCard></MenuItemCard>
-        <MenuItemCard></MenuItemCard>
-        <MenuItemCard></MenuItemCard>
-        <MenuItemCard></MenuItemCard>
+      <Container sx={{py:10}}>
+
+      <Breadcrumbs aria-label="breadcrumb" sx={{my:4}}>
+        <Link underline="hover" color="inherit" href="/">
+          Home
+        </Link>
+        <Typography color="text.primary" fontWeight={"bold"} fontSize={"150%"}>Menu</Typography>
+      </Breadcrumbs>
+
+        <Grid container spacing={6}>
+            {
+              new Array(20).fill((
+                <Grid xs={"auto"}>
+                  <MenuItemCard></MenuItemCard>
+                </Grid>
+              ))
+            }
+        </Grid>
       </Container>
+
+      {/* <Container sx={{ bgcolor: 'tomato', p:3}}>
+        <MenuItemCard></MenuItemCard>
+        <MenuItemCard></MenuItemCard>
+        <MenuItemCard></MenuItemCard>
+      </Container> */}
     </>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -37,7 +37,7 @@ function Navbar() {
   };
 
   return (
-    <AppBar position="static">
+    <AppBar position="fixed">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <LunchDiningIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />

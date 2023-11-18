19d31ac720c3e018b8563a01f377d59990902826
Dev Aryan Dogra <devdogra1@gmail.com>
2023-11-18 15:14:34 +0530

    Modify YourOrder component to act as cart

--- src/components/YourOrderTable/YourOrderTable.jsx
+++ src/components/YourOrderTable/YourOrderTable.jsx
@@ -7,17 +7,24 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Typography from '@mui/material/Typography'
 import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import TextField from '@mui/material/TextField';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 import { blueGrey } from '@mui/material/colors';
 
 
-function createData(name, calories, fat, ) {
+function createData(name, calories, fat,) {
   return { name, calories, fat, carbs, protein };
 }
 
 function getOrderItem(name, price, quantity) {
   return {
     item: {
-      name, 
+      name,
       price,
     },
     quantity,
@@ -32,22 +39,24 @@ const rows = [
 
 export default function YourOrderTable() {
   return (
-    <TableContainer component={Paper} sx={{maxHeight: 300}}>
-      <Table stickyHeader sx={{ minWidth: 650, "& .MuiTableRow-root:hover": {
-      backgroundColor: blueGrey[50],
-    
-    } }} size="small" aria-label="a dense table" >
+    <TableContainer component={Paper} sx={{ maxHeight: 300 }}>
+      <Table stickyHeader sx={{
+        minWidth: 650, "& .MuiTableRow-root:hover": {
+          backgroundColor: blueGrey[50],
+
+        }
+      }} size="small" aria-label="a dense table" >
         <TableHead >
           <TableRow hover>
-            <TableCell sx={{bgcolor: 'primary.light'}}>
+            <TableCell sx={{ bgcolor: 'primary.light', pl:8 }} align="start">
               <Typography variant="p" fontWeight="bold" textTransform="uppercase">Item</Typography>
             </TableCell>
 
-            <TableCell sx={{bgcolor: 'primary.light'}} align="right">
-              <Typography variant="p" fontWeight="bold"textTransform="uppercase">Quantity</Typography>
+            <TableCell sx={{ bgcolor: 'primary.light' }} align="center">
+              <Typography variant="p" fontWeight="bold" textTransform="uppercase">Quantity</Typography>
             </TableCell>
 
-            <TableCell sx={{bgcolor: 'primary.light'}} align="right">
+            <TableCell sx={{ bgcolor: 'primary.light' }} align="center">
               <Typography variant="p" fontWeight="bold" textTransform="uppercase">Price</Typography>
             </TableCell>
 
@@ -57,15 +66,44 @@ export default function YourOrderTable() {
           {rows.map((row) => (
             <TableRow
               key={row.item.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 }, hover: {
-                bgcolor:'red'
-              } }}
+              sx={{
+                '&:last-child td, &:last-child th': { border: 0 }, hover: {
+                  bgcolor: 'red'
+                }
+              }}
             >
-              <TableCell component="th" scope="row">
-                {row.item.name}
+
+              <TableCell component="th" scope="row" >
+                <Box sx={{ display: "flex", alignItems: "center",}}>
+                  <IconButton aria-label="delete" size="large">
+                    <DeleteIcon />
+                  </IconButton>
+
+                  <Typography>
+                    {row.item.name}
+                  </Typography>
+                </Box>
+
               </TableCell>
-              <TableCell align="right">{row.quantity}</TableCell>
-              <TableCell align="right">{row.quantity * row.item.price}</TableCell>
+
+              <TableCell align="right">
+                <Box sx={{ display: "flex", alignItems: "center", justifyContent:"center" }}>
+                  <IconButton>
+                    <RemoveIcon></RemoveIcon>
+                  </IconButton>
+
+                  <Typography>{row.quantity}</Typography>
+
+                  <IconButton>
+                    <AddIcon></AddIcon>
+                  </IconButton>
+                </Box>
+              </TableCell>
+
+              <TableCell align="center">
+                {row.quantity * row.item.price}
+                </TableCell>
+
             </TableRow>
           ))}
         </TableBody>
